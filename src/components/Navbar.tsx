b9fc2c2a3ed7090be05739d2c91c2470bc4965d4
willianchu <willianchu@hotmail.com>
2022-06-26 15:30:18 -0700

    add cart icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,19 @@
 import { Container, Nav, Navbar as NavbarBS } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { ReactComponent as ShoppingCart } from '../assets/shopping-cart.svg'
 
 export function Navbar() {
   return (
   <NavbarBS className="bg-white shadow-lg mb-3">
     <Container>
-    <Nav>
+    <Nav className='me-auto'>
       <Nav.Link to="/"as ={NavLink}>Home</Nav.Link>
       <Nav.Link to="/store"as ={NavLink}>Store</Nav.Link>
       <Nav.Link to="/about"as ={NavLink}>About</Nav.Link>
     </Nav>
+    <button>
+      <ShoppingCart />
+    </button>
     </Container> 
   </NavbarBS>
   )
