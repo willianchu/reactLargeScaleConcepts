13dbc9f4163d97393f7ecdb1c1560fa09b78eb93
willianchu <willianchu@hotmail.com>
2022-06-28 19:15:40 -0700

    stringify item

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,12 +1,13 @@
 import { Col, Row } from 'react-bootstrap';
 import storeItems from '../data/items.json';
+
 export function Store() {
   return (
     <>
       <h1>Store</h1>
       <Row>
         {storeItems.map((item) => (
-        <Col>JSON.stringify(item)</Col>
+        <Col>{JSON.stringify(item)}</Col>
         ))}
       </Row>
     </>
