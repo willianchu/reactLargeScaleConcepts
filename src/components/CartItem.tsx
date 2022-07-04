b6db90ae429004e6e50a1e0359114583dccba73d
willianchu <willianchu@hotmail.com>
2022-07-04 11:23:11 -0700

    sub totals in sidebar added

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../data/items.json";
 import { formatCurrency } from "../utilities/formatCurrency";
@@ -28,10 +28,11 @@ export function CartItem({ id, quantity }: CartItemProps) {
         <div className="text-muted" style={{ fontSize: ".75rem" }}>
           {formatCurrency(item.price)}
         </div>
+      </div>
         <div>
           {formatCurrency(item.price * quantity)}
         </div>
-      </div>
+        <Button variant="outline-danger" size="sm" onClick={() => removeFromCart(item.id)}>&times;</Button>
     </Stack>
   );
 }
