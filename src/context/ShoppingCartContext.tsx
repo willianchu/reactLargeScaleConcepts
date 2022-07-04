391c3a8a04d0d0fb2d7409aff7e744e673cb3765
willianchu <willianchu@hotmail.com>
2022-07-04 08:53:43 -0700

    removefrom cart  function

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -61,8 +61,14 @@ export function ShoppingCartProvider({ children } : ShoppingCartProviderProps) {
       })
     };
 
+    function removeFromCart(id: number) {
+      setCartItems(currItems => {
+        return currItems.filter(item => item.id !== id);
+      })
+    }
+
   return (
-  <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity}}>
+  <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
     {children}
   </ShoppingCartContext.Provider>
   )
