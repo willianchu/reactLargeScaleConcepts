7ca6d925319b05b8157afd167c378ec8c19775bb
willianchu <willianchu@hotmail.com>
2022-07-04 11:04:48 -0700

    add X quantity in sidebar

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -13,8 +13,18 @@ export function CartItem({ id, quantity }: CartItemProps) {
   if (item == null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img src={ item.imgUrl } alt={ item.name } style={{ width: "125px", height: "75px" , objectFit: "cover"}} />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}	
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem"}}>
+              x{quantity}
+            </span>
+          )}
+        </div>  
+      </div>
     </Stack>
   );
 }
