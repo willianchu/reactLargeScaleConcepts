0575670a61ffb8b4a234bab167c8c2b1f3d9f6f4
willianchu <willianchu@hotmail.com>
2022-07-04 10:31:58 -0700

    add a shopping cart component

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
   children: ReactNode;
@@ -84,12 +85,14 @@ export function ShoppingCartProvider({ children } : ShoppingCartProviderProps) {
         increaseCartQuantity,
         decreaseCartQuantity,
         removeFromCart,
-        openCart, closeCart,
+        openCart,
+        closeCart,
         cartItems,
         cartQuantity
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   )
 };
