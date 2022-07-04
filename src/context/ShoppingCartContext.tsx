2516391ff521dd4e1e1ddd0221cf02fdfdb2ae90
willianchu <willianchu@hotmail.com>
2022-07-03 21:52:21 -0700

    increase function

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,7 +1,12 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 type ShoppingCartProviderProps = {
   children: ReactNode;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
 };
 
 type ShoppingCartContext = {
@@ -18,9 +23,31 @@ export function useShoppingCart() {
 };
 
 export function ShoppingCartProvider({ children } : ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(itemId: number) {
+    return cartItems.find(item => item.id === itemId)?.quantity || 0;
+  }
+
+    function increaseCartQuantity(itemId: number) {
+      setCartItems(cartItems => {
+        if (currItems.find(item => item.id === id) == null){
+          return [...currItems, { id, quantity: 1 }];
+        } else {
+          return currItems.map(item => {
+            if (item.id === id) {
+              return { ...item, quantity: item.quantity + 1 };
+            } else {
+              return item;
+            }
+          });
+        }
+      })
+    };
+
   return (
-    <ShoppingCartContext.Provider value={{}}>
-      {children}
-    </ShoppingCartContext.Provider>
+  <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity}}>
+    {children}
+  </ShoppingCartContext.Provider>
   )
 };
